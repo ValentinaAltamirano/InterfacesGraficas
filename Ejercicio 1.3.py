04c5b152f9f98e0b0c8863579f16738791e086bf
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-19 10:17:24 -0300

    Update Ejercicio 1.3.py

--- Ejercicio 1.3.py
+++ Ejercicio 1.3.py
@@ -22,27 +22,15 @@ class Factorial(tk.Tk):
         self.num.config(state=tk.NORMAL)
         self.num2.config(state=tk.NORMAL)
         num = self.num.get()
-        numlen = len(str(num))
-        if numlen == 0:
-            numN = 0
-            numN += 1
-            self.num.delete(0, END)
-            self.num.insert(0, str(numN))
-            f = self.factor(numN)
-            self.num2.delete(0, END)
-            self.num2.insert(0, str(f))
-            self.num.config(state= 'readonly')
-            self.num2.config(state= 'readonly')
-        else:
-            num = int(num)
-            num += 1
-            self.num.delete(0, END)
-            self.num.insert(0, str(num))
-            f = self.factor(num)
-            self.num2.delete(0, END)
-            self.num2.insert(0, str(f))
-            self.num.config(state= 'readonly')
-            self.num2.config(state= 'readonly')
+        num = int(num)
+        num += 1
+        self.num.delete(0, END)
+        self.num.insert(0, str(num))
+        f = self.factor(num)
+        self.num2.delete(0, END)
+        self.num2.insert(0, str(f))
+        self.num.config(state= 'readonly')
+        self.num2.config(state= 'readonly')
         
             
     def factor(self,n):
@@ -53,14 +41,18 @@ class Factorial(tk.Tk):
         self.n = Label(self, text="n")
         self.n.grid(column=0, row=0, sticky=tk.E, padx=5, pady=5)
 
-        self.num = Entry(self,state= 'readonly')
+        self.num = Entry(self)
         self.num.grid(column=1, row=0, sticky=tk.W, padx=0, pady=5)
+        self.num.insert(0, '1')
+        self.num.config(state= 'readonly')
 
         self.factorial = Label(self, text="Factorial (n)")
         self.factorial.grid(column=0, row=1, sticky=tk.E, padx=5, pady=5)
 
-        self.num2 = Entry(self,state= 'readonly')
+        self.num2 = Entry(self)
         self.num2.grid(column=1, row=1, sticky=tk.W, padx=0, pady=5)
+        self.num2.insert(0, '1')
+        self.num2.config(state= 'readonly')
 
         self.siguiente = Button(self, text="Siguiente",command=lambda: self.numero())
         self.siguiente.grid(column=1, row=2, sticky=tk.EW, padx=5, pady=5)
