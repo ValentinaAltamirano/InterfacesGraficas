49303bac313ac6890ed4918c2f88fee308d484ca
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-19 11:30:38 -0300

    Update Ejercicio 2.2.py

--- Ejercicio 2.2.py
+++ Ejercicio 2.2.py
@@ -29,26 +29,26 @@ class Contador(Frame):
     def createWidgets(self):
 
         self.p = Label(self, text="Escribe el titulo de la película:")
-        self.p.grid(row=1, column=1, sticky="sew")
+        self.p.grid(row=1, column=1, sticky="sew", padx = 15)
 
         self.peli = Entry(self)
-        self.peli.grid(row=2, column=1, sticky="sew")
+        self.peli.grid(row=2, column=1, sticky="sew", padx = 15)
 
         self.añadir = Button(self, text="Añadir",command=lambda: self.añadirPelicula())
-        self.añadir.grid(row=3, column=1, sticky="sew")
+        self.añadir.grid(row=3, column=1, sticky="sew", padx = 15)
 
         self.p = Label(self, text="Peliculas")
-        self.p.grid(row=2, column=2, sticky="sew")
+        self.p.grid(row=2, column=2, sticky="sew", padx = 5)
 
         self.lista = Listbox(self)
         self.lista.insert(0, 'Tom y Jerry')
         self.lista.insert(1, '7 Almas')
-        self.lista.grid(row=2, column=5, sticky="sew")
+        self.lista.grid(row=0, column=5, sticky=EW, rowspan=5,pady = 5)
         
 ventana = Tk()
 ventana.title("Películas")
 ventana.resizable(False, False)
-ventana.geometry("400x260")
+ventana.geometry("400x200")
 root = Contador(ventana).grid()
 
 ventana.mainloop()
