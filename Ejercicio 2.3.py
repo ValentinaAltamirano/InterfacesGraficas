1299ed03410f7076dd4827777a17b2a219810e36
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-19 10:48:14 -0300

    Update Ejercicio 2.3.py

--- Ejercicio 2.3.py
+++ Ejercicio 2.3.py
@@ -5,8 +5,9 @@
 from tkinter import *
 import tkinter as tk
 import random
+from tkinter import messagebox
 
-class Contador(tk.Tk):
+class Generador(tk.Tk):
 
     def __init__(self):
         super().__init__()
@@ -17,37 +18,51 @@ class Contador(tk.Tk):
         self.columnconfigure(1, weight=3)
         self.Widgets()
 
+    def validar(self,n):
+        try:
+            int(n)
+            return True
+        except:
+            return False  
+
     def generarNumero(self):
         self.res.config(state=tk.NORMAL)
-        n1 = int(self.entrada1.get())
-        n2 = int(self.entrada2.get())
-        num = random.randint(n1, n2)
-        self.res.delete(0, END)
-        self.res.insert(0, str(num))
-        self.res.config(state=tk.DISABLED)
+        n1 = self.entrada1.get()
+        n2 = self.entrada2.get()
+        if self.validar(n1) and self.validar(n2):
+            if int(n1) < int(n2):
+                num = random.randint(int(n1), int(n2))
+                self.res.delete(0, END)
+                self.res.insert(0, str(num))
+                self.res.config(state='readonly')
+            else:
+                messagebox.showinfo(message="Fuera de rango, el primer numero debe ser menor que el segundo.", title="Error")
             
 
     def Widgets(self):
         self.e = Label(self, text="Primer número")
         self.e.grid(column=0, row=0, sticky=tk.E, padx=5, pady=10)
 
-        self.entrada1 = Spinbox(self)
+        self.entrada1 = Spinbox(from_=0, to=1000, increment=1, state="readonly")
         self.entrada1.grid(column=1, row=0, sticky=tk.W, padx=0, pady=10)
+        self.entrada1.insert(0, '18')
 
         self.e2 = Label(self, text="Segundo número")
         self.e2.grid(column=0, row=1, sticky=tk.E, padx=5, pady=10)
 
-        self.entrada2 = Spinbox(self)
+        self.entrada2 = Spinbox(from_=0, to=1000, increment=1, state="readonly")
         self.entrada2.grid(column=1, row=1, sticky=tk.W, padx=0, pady=10)
+        self.entrada2.insert(0, '18')
+
 
         self.r = Label(self, text="Número generado")
         self.r.grid(column=0, row=3, sticky=tk.E, padx=5, pady=10) 
 
-        self.res = Entry(self,state= tk.DISABLED)
+        self.res = Entry(self,state= 'readonly')
         self.res.grid(column=1, row=3, sticky=tk.W, padx=0, pady=5) 
 
         self.gen = Button(self,text="Añadir",command=lambda: self.generarNumero())
         self.gen.grid(column=1, row=4, sticky=tk.SW, padx=35, pady=5)
 
-root = Contador()
+root = Generador()
 root.mainloop()
