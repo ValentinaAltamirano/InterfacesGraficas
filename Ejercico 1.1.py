4d5ff9c86215769d16611470d915c9dbd66d467b
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-19 10:14:30 -0300

    Update Ejercico 1.1.py

--- Ejercico 1.1.py
+++ Ejercico 1.1.py
@@ -2,10 +2,6 @@
 # ve en la figura. Cada vez que se haga clic en el botón "+", el valor del
 # contador se incrementa en 1.
 
-# Escribir una aplicación GUI (llamada ContCreciente) como la que se
-# ve en la figura. Cada vez que se haga clic en el botón "+", el valor del
-# contador se incrementa en 1.
-
 from tkinter import *
 import tkinter as tk
 
@@ -24,19 +20,11 @@ class Contador(tk.Tk):
     def sumar(self):
         self.entrada.config(state=tk.NORMAL)
         num = self.entrada.get()
-        numlen = len(str(num))
-        if numlen == 0:
-            numN = 0
-            numN += 1
-            self.entrada.delete(0, END)
-            self.entrada.insert(0, str(numN))
-            self.entrada.config(state='readonly')
-        else:
-            num = int(num)
-            num += 1
-            self.entrada.delete(0, END)
-            self.entrada.insert(0, str(num))
-            self.entrada.config(state='readonly')
+        num = int(num)
+        num += 1
+        self.entrada.delete(0, END)
+        self.entrada.insert(0, str(num))
+        self.entrada.config(state='readonly')
             
 
     def Widgets(self):
@@ -44,12 +32,13 @@ class Contador(tk.Tk):
         self.cont = Label(self, text="Contador:")
         self.cont.grid(column=0, row=1, sticky=tk.E, padx=5, pady=10)
 
-        self.entrada = Entry(self,state= 'readonly')
+        self.entrada = Entry(self)
         self.entrada.grid(column=1, row=1, sticky=tk.W, padx=0, pady=10)
+        self.entrada.insert(0, '18')
+        self.entrada.config(state= 'readonly')
 
         self.mas = Button(self, text="+",command=lambda: self.sumar())
         self.mas.grid(column=2, row=1, sticky=tk.W, padx=5, pady=10)
         
 root = Contador()
 root.mainloop()
-
