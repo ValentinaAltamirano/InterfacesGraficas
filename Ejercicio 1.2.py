5313da2b551a2618eddcd2dcfee7861a981912a6
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-09-19 10:12:23 -0300

    Update Ejercicio 1.2.py

--- Ejercicio 1.2.py
+++ Ejercicio 1.2.py
@@ -16,23 +16,15 @@ class Contador(tk.Tk):
         self.columnconfigure(2, weight=3)
         self.rowconfigure(1, weight=5)
         self.Widgets()
-#revisar
+        
     def restar(self):
         self.entrada.config(state=tk.NORMAL)
         num = self.entrada.get()
-        numlen = len(str(num))
-        if numlen == 0:
-            numN = 0
-            numN -= 1
-            self.entrada.delete(0, END)
-            self.entrada.insert(0, str(numN))
-            self.entrada.config(state= 'readonly')
-        else:
-            num = int(num)
-            num -= 1
-            self.entrada.delete(0, END)
-            self.entrada.insert(0, str(num))
-            self.entrada.config(state= 'readonly')
+        num = int(num)
+        num -= 1
+        self.entrada.delete(0, END)
+        self.entrada.insert(0, str(num))
+        self.entrada.config(state= 'readonly')
             
 
     def Widgets(self):
@@ -40,8 +32,10 @@ class Contador(tk.Tk):
         self.cont = Label(self, text="Contador:")
         self.cont.grid(column=0, row=1, sticky=tk.E, padx=5, pady=10)
 
-        self.entrada = Entry(self,state= 'readonly')
+        self.entrada = Entry(self)
         self.entrada.grid(column=1, row=1, sticky=tk.W, padx=0, pady=10)
+        self.entrada.insert(0, '88')
+        self.entrada.config(state= 'readonly')
 
         self.menos = Button(self, text="-",command=lambda: self.restar())
         self.menos.grid(column=2, row=1, sticky=tk.W, padx=5, pady=10)
@@ -51,3 +45,4 @@ root.mainloop()
 
 #apenas ejecuto no trae ningun nº por defecto por lo tanto cuando empiezo a clickear comienza desde 0 y va hacia los -1. ·
 #En la consigna el valor por defecto inicial es 88 si mal lo recuerdo
+#Corregido!
